439aa2622ddebba114541a775daef5fa96dd23b8
Peter <pfirlotte@academic.rrc.ca>
2025-04-22 13:13:50 -0500

    Added form validation.

--- script.js
+++ script.js
@@ -18,3 +18,95 @@ form.addEventListener("submit", (event) => {
         console.error("Form has errors");
     }
 });
+
+// different input checking for the form
+function validateForm() {
+    let isFormValid = true;
+
+// Checks that the first name is not empty or numbers
+    const userName = document.getElementById("name");
+
+    if (userName.value === "") {
+        showInputError(userName, "Please enter a first name. ");
+        isFormValid = false;
+    }
+
+    else if (!isNaN(userName.value)) {
+        showInputError(userName, "Please use letters. ");
+        isFormValid = false;
+    }
+// Validates the email entered
+
+    const emailInputElement = document.getElementById("email");
+    const emailInputValue = emailInputElement.value;
+
+    const emailPattern = /.+@.+\..+/;
+
+    if (emailInputElement.value === "") {
+        showInputError(emailInputElement, "Please enter an email address. ");
+        isFormValid = false;
+    }
+
+    else if (!emailPattern.test(emailInputValue)) {
+        showInputError(emailInputElement, "Please enter a valid email address.");
+        isFormValid = false;
+    }
+
+// Checks that the first name is not empty or numbers
+const charityName = document.getElementById("name");
+
+if (charityName.value === "") {
+    showInputError(charityName, "Please enter a Charity name. ");
+    isFormValid = false;
+}
+
+else if (!isNaN(charityName.value)) {
+    showInputError(charityName, "Please use letters. ");
+    isFormValid = false;
+}
+
+// Validates Radio Buttons
+    const radioSelected = document.getElementById("option1"); 
+    const radioSelected2 = document.getElementById("option2");
+    const radioSelected3 = document.getElementById("option3");
+    const radioSelected4 = document.getElementById("option4");
+    const radioSelected5 = document.getElementById("option5");
+
+    if (!radioSelected.checked && !radioSelected2.checked) {
+        showInputError(radioSelected,"Please select an option.");
+        isFormValid = false;
+    }
+
+// Checks that the quantity is not empty or letters
+
+    const amountNumber = document.getElementById("quantity");
+
+    if (amountNumber.value === "") {
+        showInputError(amountNumber, "Please enter an number. ");
+        isFormValid = false;
+    }
+
+    return isFormValid;
+}
+
+// Appends the error messages
+function showInputError(inputElement, message) {
+    const container = inputElement.closest(".input-container");
+    const errorDisplay = document.createElement("span");
+    errorDisplay.innerText = message;
+    errorDisplay.className = "error-message";
+    errorDisplay.setAttribute("role", "alert");
+
+    container.appendChild(errorDisplay);
+}
+
+form.addEventListener("reset", (event) => {
+
+    const errorMessages = document.querySelectorAll(".error-message");
+    for (const el of errorMessages) {
+        el.remove();
+    }
+
+    form.reset();
+
+});
