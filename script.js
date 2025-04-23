53c3d1e97a66bd3ad6ac6c43d0792b19a71613c1
Ian <ilamb@rrc.ca>
2025-04-22 19:59:04 -0500

    Found an error for peter to fix.

--- script.js
+++ script.js
@@ -29,33 +29,3 @@ document.getElementById("donationForm").addEventListener("submit", (event) => {
 
     console.log("Donation Data", donationData)
 });
-
-const form = document.getElementById("submissionForm");
-
-document.getElementById("submissionForm").addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const userName = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const emailPattern = /.+@.+\..+/;
-    const charityName= document.getElementById("charity-name").value.trim();
-    const experienceRating = document.querySelector('input[name="options"]:checked').value;
-    const volunteerHours = document.getElementById("hours-quantity").value
-    const volunteerDate = document.getElementById("specificDate").value
-
-    if (!userName || !isNaN(userName) || !email || emailPattern.test(email) || !charityName || !experienceRating 
-        || experienceRating === null || isNaN(volunteerHours) || !volunteerDate ) {
-        alert("Please fill out all required fields correctly. Volunteer Hours amount must be a positive number.");
-        return;
-    }
-
-    const charitySubmission = { "userName": `${userName}`, "userEmail": `${email}`,
-        "charityName": `${charityName}`, "rating": `${experienceRating}`,
-        "volunteerHours": `${volunteerHours}`, "volunteerDate": `${volunteerDate}`}
-
-    console.log(charitySubmission)
-});
-
-form.addEventListener("reset", (event) => {
-    form.reset();
-});
