1676788cae4d22ed8a4931f5cac49367fa1729e3
Ian <ilamb@rrc.ca>
2025-04-24 18:12:11 -0500

    Implemented table to show volunteer history.

--- volunteer_hours_script.js
+++ volunteer_hours_script.js
@@ -1,5 +1,7 @@
 const form = document.getElementById("submissionForm");
 
+loadVolunteerHistory();
+
 document.getElementById("submissionForm").addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -17,25 +19,45 @@ document.getElementById("submissionForm").addEventListener("submit", (event) => 
     }
     saveVolunteerEntry(userName, email, charityName, volunteerHours,
                        volunteerDate, experienceRating);
+    loadVolunteerHistory();
 });
 
 form.addEventListener("reset", () => {
     form.reset();
 });
 
+// function saveVolunteerEntry(username, email, charity, hoursWorked, dateWorked, rating){
+//     if(localStorage.getItem("volunteerEntries") === null)
+//     {
+//         localStorage.setItem("volunteerEntries", `{"volunteerEntries":
+//             [{"username":"${username}","email":"${email}",
+//             "charity":"${charity}","hoursWorked":"${hoursWorked}",
+//             "dateWorked":"${dateWorked}","rating":"${rating}"}]}`);
+//     }
+//     else{
+//         let jsonString = localStorage.getItem("volunteerEntries");
+//         let jsonObject = JSON.parse(jsonString);
+//         let newJsonEntry = JSON.parse(`{"username":"${username}","email":"${email}",
+//                                       "charity":"${charity}","hoursWorked":"${hoursWorked}",
+//                                       "dateWorked":"${dateWorked}","rating":"${rating}"}`);
+
+//         jsonObject.volunteerEntries.push(newJsonEntry);
+//         console.log(jsonObject.volunteerEntries);
+//         localStorage.setItem("volunteerEntries", JSON.stringify(jsonObject));
+//     }
+// }
+
 function saveVolunteerEntry(username, email, charity, hoursWorked, dateWorked, rating){
     if(localStorage.getItem("volunteerEntries") === null)
     {
         localStorage.setItem("volunteerEntries", `{"volunteerEntries":
-            [{"username":"${username}","email":"${email}",
-            "charity":"${charity}","hoursWorked":"${hoursWorked}",
+            [{"charity":"${charity}","hoursWorked":"${hoursWorked}",
             "dateWorked":"${dateWorked}","rating":"${rating}"}]}`);
     }
     else{
         let jsonString = localStorage.getItem("volunteerEntries");
         let jsonObject = JSON.parse(jsonString);
-        let newJsonEntry = JSON.parse(`{"username":"${username}","email":"${email}",
-                                      "charity":"${charity}","hoursWorked":"${hoursWorked}",
+        let newJsonEntry = JSON.parse(`{"charity":"${charity}","hoursWorked":"${hoursWorked}",
                                       "dateWorked":"${dateWorked}","rating":"${rating}"}`);
 
         jsonObject.volunteerEntries.push(newJsonEntry);
@@ -51,4 +73,29 @@ function loadVolunteerHistory(){
         return;
 
     let volunteerHistory = JSON.parse(volunteerEntryObject).volunteerEntries;
+
+    const table = document.getElementById("history-logs");
+    table.innerHTML = "";
+
+    for(let i = 0; i < volunteerHistory.length; i++){
+        const tableEntry = document.createElement("tr");
+        
+        for(const key in volunteerHistory[i]){
+            const value = volunteerHistory[i][key];
+            const charityItem = document.createElement("td");
+            charityItem.innerText = value;
+            
+            tableEntry.appendChild(charityItem);
+        }
+
+        const deleteButton = document.createElement("button")
+        deleteButton.innerText = "Delete"
+        tableEntry.appendChild(deleteButton);
+
+        deleteButton.addEventListener("click", () =>{
+            tableEntry.remove();
+        })
+
+        table.appendChild(tableEntry)
+    }
 }
