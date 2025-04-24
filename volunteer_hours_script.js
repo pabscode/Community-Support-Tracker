1fcdcf3b9dd7bc0f082a77b5edf7da025fbf313f
Ian <ilamb@rrc.ca>
2025-04-24 16:10:21 -0500

    Implemented saving volunteer entries to localstorage.

--- volunteer_hours_script.js
+++ volunteer_hours_script.js
@@ -5,25 +5,50 @@ document.getElementById("submissionForm").addEventListener("submit", (event) => 
 
     const userName = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
-    const emailPattern = /.+@.+\..+/;
     const charityName= document.getElementById("charity-name").value.trim();
     const experienceRating = document.querySelector('input[name="options"]:checked').value;
-    const volunteerHours = document.getElementById("hours-quantity").value
-    const volunteerDate = document.getElementById("specificDate").value
+    const volunteerHours = document.getElementById("hours-quantity").value;
+    const volunteerDate = document.getElementById("specificDate").value;
 
-    if (!userName || !isNaN(userName) || !email || emailPattern.test(email) || !charityName || !experienceRating 
+    if (!userName || !isNaN(userName) || !email || !charityName || !experienceRating 
         || experienceRating === null || isNaN(volunteerHours) || !volunteerDate ) {
         alert("Please fill out all required fields correctly. Volunteer Hours amount must be a positive number.");
         return;
     }
-
-    const charitySubmission = { "userName": `${userName}`, "userEmail": `${email}`,
-        "charityName": `${charityName}`, "rating": `${experienceRating}`,
-        "volunteerHours": `${volunteerHours}`, "volunteerDate": `${volunteerDate}`}
-
-    console.log(charitySubmission)
+    saveVolunteerEntry(userName, email, charityName, volunteerHours,
+                       volunteerDate, experienceRating);
 });
 
-form.addEventListener("reset", (event) => {
+form.addEventListener("reset", () => {
     form.reset();
 });
+
+function saveVolunteerEntry(username, email, charity, hoursWorked, dateWorked, rating){
+    if(localStorage.getItem("volunteerEntries") === null)
+    {
+        localStorage.setItem("volunteerEntries", `{"volunteerEntries":
+            [{"username":"${username}","email":"${email}",
+            "charity":"${charity}","hoursWorked":"${hoursWorked}",
+            "dateWorked":"${dateWorked}","rating":"${rating}"}]}`);
+    }
+    else{
+        let jsonString = localStorage.getItem("volunteerEntries");
+        let jsonObject = JSON.parse(jsonString);
+        let newJsonEntry = JSON.parse(`{"username":"${username}","email":"${email}",
+                                      "charity":"${charity}","hoursWorked":"${hoursWorked}",
+                                      "dateWorked":"${dateWorked}","rating":"${rating}"}`);
+
+        jsonObject.volunteerEntries.push(newJsonEntry);
+        console.log(jsonObject.volunteerEntries);
+        localStorage.setItem("volunteerEntries", JSON.stringify(jsonObject));
+    }
+}
+
+function loadVolunteerHistory(){
+    let volunteerEntryObject = localStorage.getItem("volunteerEntries");
+
+    if(volunteerEntryObject === null)
+        return;
+
+    let volunteerHistory = JSON.parse(volunteerEntryObject).volunteerEntries;
+}
